72e5cdfc15c3c633fcb512aaae80b45c0e794c89
DanielYouCan <danbizarre@gmail.com>
2017-10-27 18:00:50 +0700

    Acerule modified, dealer_move method added and some corrections

--- BlackJack/acerule.rb
+++ BlackJack/acerule.rb
@@ -1,6 +1,7 @@
 module AceRule
   def check_ace(player)
-    if player.points > 20 && (player.cards & %w(A+ A<> A<3 A^))
+    ace_on_hands = player.cards & %w(A+ A<> A<3 A^)
+    if player.points > 21 && !(ace_on_hands.empty?)
       player.points -= 10
     end
   end
--- BlackJack/main.rb
+++ BlackJack/main.rb
@@ -45,6 +45,7 @@ class Game
     @player.cards = @desc.shift(2)
     @dealer.cards = @desc.shift(2)
     count_points(@player)
+    count_points(@dealer)
     check_ace(@dealer)
     puts "#{player_name}'s cards: #{@player.cards} points: #{@player.points}"
     puts "Dealer's cards **"
@@ -52,13 +53,14 @@ class Game
       options
       move = gets.chomp
       read(move)
+      dealer_move
     end
   end
 
   private
 
   def options
-    if @card_added
+    if @player.card_added?
       puts "Choose what you'd like to do:
       0 - skip a move
       2 - open cards"
@@ -70,7 +72,14 @@ class Game
     end
   end
 
-  def dealer_logic
+  def dealer_move
+    puts "Dealer's move:"
+    if @dealer.points >= 18
+      skip_move
+    elsif @dealer.points < 18 && (@dealer.points < @player.points)
+      puts "Dealer took a card"
+      add_card(@dealer)
+    end
   end
 
   def count_points(player)
@@ -80,7 +89,7 @@ class Game
   end
 
   def skip_move
-    puts "You've passed a move to another player"
+    puts "Move is passed to another player"
   end
 
   def add_card(player)
@@ -88,21 +97,22 @@ class Game
     player.cards << @desc.shift
     player.points += VALUES[player.cards[2]]
     check_ace(player)
-    puts "Your cards: #{player.cards} points: #{player.points}"
+
+    puts "Your cards: #{player.cards} points: #{player.points}" if player == @player
   end
 
   def open_cards
-    puts "dealer's cards: #{@dealer.cards} points: #{@dealer_points}"
-    if @dealer_points > @player_points && @dealer_points < 22
+    puts "dealer's cards: #{@dealer.cards} points: #{@dealer.points}"
+    if @dealer.points > @player.points && @dealer.points < 22
       @dealer.bankroll += @bank
       @bank = 0
       puts "You've lost this round"
-    elsif @dealer_points == @player_points
+    elsif @dealer.points == @player.points
       @dealer.bankroll += @bank/2
       @player.bankroll += @bank/2
       @bank = 0
       puts "That's a draw"
-    elsif @dealer_points < @player_points && @player_points < 22
+    elsif @dealer.points < @player.points && @player.points < 22
       @player.bankroll += @bank
       @bank = 0
       puts "You've won!"
@@ -114,7 +124,7 @@ class Game
     when "0"
       skip_move
     when "1"
-      @player.card_added? ? puts "You can add card only one time" : add_card(@player)
+      @player.card_added? ? "You can add card only one time" : add_card(@player)
     when "2"
       open_cards
     when "finish"
